c7a1628c9ec9bf27a779ee3330189d7d64e23e4a
adityachirania <52464668+adityachirania@users.noreply.github.com>
2019-12-23 18:02:24 +0530

    Will have IR validations on front end

--- app/models/report.rb
+++ app/models/report.rb
@@ -1,9 +1,3 @@
 class Report < ApplicationRecord
   belongs_to :audit
-  validate :emp_check
-  private def emp_check
-    if !(Employee.find(vs_auditor_id))
-      errors[:base] << " doesnt exist in the employees")
-    end
-  end
 end
